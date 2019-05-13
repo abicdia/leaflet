d86f92420d1653b345d47d8ada58ee5780d754b4
abicdia <237406984@qq.com>
2019-05-13 10:28:00 +0800

    新增气温数据展示和解析

---
+++ json/data.js
@@ -0,0 +1,4 @@
+var statesData = {"type":"FeatureCollection","features":[
+    {"type":"Feature","id":"58457","properties":{"name":"杭州","density":20.2},"geometry":{"type":"Point","coordinates":[120, 31.505]}},
+    {"type":"Feature","id":"58457","properties":{"name":"宝山","density":21.9},"geometry":{"type":"Point","coordinates":[121.45, 31.4]}},
+   ]};
